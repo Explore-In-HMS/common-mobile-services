56f686fb5cfb9e048b98ff75474dedc8e527be6b
mustafadincay <mustafadincay@gmail.com>
2022-12-01 11:37:51 +0300

    feat: add Object Detection for Huawei

---
+++ objectdetection/src/main/java/com/hms/lib/commonmobileservices/objectdetection/HuaweiObjectDetectionKit.kt
@@ -0,0 +1,66 @@
+// Copyright 2020. Explore in HMS. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.hms.lib.commonmobileservices.objectdetection
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import androidx.core.app.ActivityCompat
+import com.hms.lib.commonmobileservices.core.ResultData
+import com.hms.lib.commonmobileservices.objectdetection.manager.IObjectDetectionAPI
+import com.huawei.hms.mlsdk.MLAnalyzerFactory
+import com.huawei.hms.mlsdk.common.MLApplication
+import com.huawei.hms.mlsdk.common.MLFrame
+import com.huawei.hms.mlsdk.objects.MLObjectAnalyzerSetting
+
+class HuaweiObjectDetectionKit : IObjectDetectionAPI {
+
+    override fun staticImageDetection(
+        callback: (detectedValue: ResultData<List<Any>>) -> Unit,
+        activity: Activity,
+        bitmap: Bitmap,
+        hmsApiKey: String
+    ) {
+        if (ActivityCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            MLApplication.getInstance().apiKey = hmsApiKey
+            val setting = MLObjectAnalyzerSetting.Factory()
+                .setAnalyzerType(MLObjectAnalyzerSetting.TYPE_PICTURE)
+                .allowMultiResults()
+                .allowClassification()
+                .create()
+            val analyzer = MLAnalyzerFactory.getInstance().getLocalObjectAnalyzer(setting)
+
+            val frame = MLFrame.fromBitmap(bitmap)
+
+            val task = analyzer!!.asyncAnalyseFrame(frame)
+
+            task?.addOnSuccessListener {
+                callback.invoke(ResultData.Success(it))
+            }?.addOnFailureListener {
+                callback.invoke(ResultData.Failed())
+            }
+        } else {
+            val strings = arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            )
+            ActivityCompat.requestPermissions(activity, strings, 2)
+        }
+    }
+}
