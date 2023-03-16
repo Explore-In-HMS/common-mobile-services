3e730768623bcaeb306b83eb724c94bc95f4c04b
mertkesgin <mertkesgin@yahoo.com>
2023-03-16 11:01:43 +0300

    Implement huawei translate

--- translate/src/main/java/com/hms/lib/commonmobileservices/translate/implementation/HuaweiTranslator.kt
+++ translate/src/main/java/com/hms/lib/commonmobileservices/translate/implementation/HuaweiTranslator.kt
@@ -4,22 +4,53 @@ import com.hms.lib.commonmobileservices.translate.common.DeleteModelResult
 import com.hms.lib.commonmobileservices.translate.common.DownloadModelResult
 import com.hms.lib.commonmobileservices.translate.common.RequiresModelDownloadResult
 import com.hms.lib.commonmobileservices.translate.common.TranslateResult
+import com.huawei.hms.mlsdk.model.download.MLLocalModelManager
+import com.huawei.hms.mlsdk.model.download.MLModelDownloadListener
+import com.huawei.hms.mlsdk.model.download.MLModelDownloadStrategy
+import com.huawei.hms.mlsdk.translate.MLTranslatorFactory
+import com.huawei.hms.mlsdk.translate.local.MLLocalTranslateSetting
+import com.huawei.hms.mlsdk.translate.local.MLLocalTranslator
+import com.huawei.hms.mlsdk.translate.local.MLLocalTranslatorModel
 
 class HuaweiTranslator : ITranslator {
+
+    private val modelManager = MLLocalModelManager.getInstance()
+    private lateinit var translator: MLLocalTranslator
+
     override fun translate(
         text: String,
         sourceLanguage: String,
         targetLanguage: String,
         callback: (translateResult: TranslateResult) -> Unit
     ) {
-        TODO("Not yet implemented")
+        val setting = MLLocalTranslateSetting.Factory()
+            .setSourceLangCode(sourceLanguage)
+            .setTargetLangCode(targetLanguage)
+            .create()
+        translator = MLTranslatorFactory.getInstance().getLocalTranslator(setting)
+
+        translator.asyncTranslate(text)
+            .addOnSuccessListener { translatedText ->
+                callback(TranslateResult.Success(translatedText))
+            }
+            .addOnFailureListener { exception ->
+                callback(TranslateResult.Error(exception))
+            }
     }
 
     override fun requiresModelDownload(
         langCode: String,
         callback: (requiresModelDownloadResult: RequiresModelDownloadResult) -> Unit
     ) {
-        TODO("Not yet implemented")
+        val model = MLLocalTranslatorModel.Factory(langCode).create()
+        modelManager.isModelExist(model)
+            .addOnSuccessListener { modelExist ->
+                if (!modelExist) callback(RequiresModelDownloadResult.Required)
+                else callback(RequiresModelDownloadResult.NotRequired)
+            }
+            .addOnFailureListener { exception ->
+                callback(RequiresModelDownloadResult.Error(exception))
+            }
     }
 
     override fun downloadModel(
@@ -27,17 +58,37 @@ class HuaweiTranslator : ITranslator {
         requireWifi: Boolean,
         callback: (downloadResult: DownloadModelResult) -> Unit
     ) {
-        TODO("Not yet implemented")
+        val model = MLLocalTranslatorModel.Factory(langCode).create()
+        val downloadStrategyFactory = MLModelDownloadStrategy.Factory()
+        if (requireWifi){
+            downloadStrategyFactory.needWifi()
+        }
+        val downloadStrategy = downloadStrategyFactory.create()
+        val modelDownloadListener = MLModelDownloadListener { alreadyDownLength, totalLength -> }
+
+        modelManager.downloadModel(model, downloadStrategy, modelDownloadListener)
+            .addOnSuccessListener {
+                callback(DownloadModelResult.Success)
+            }.addOnFailureListener { exception ->
+                callback(DownloadModelResult.Error(exception))
+            }
     }
 
     override fun deleteModel(
         langCode: String,
         callback: (downloadResult: DeleteModelResult) -> Unit
     ) {
-        TODO("Not yet implemented")
+        val model = MLLocalTranslatorModel.Factory(langCode).create()
+        modelManager.deleteModel(model)
+            .addOnSuccessListener {
+                callback(DeleteModelResult.Success)
+            }.addOnFailureListener { exception ->
+                callback(DeleteModelResult.Error(exception))
+            }
     }
 
     override fun close() {
-        TODO("Not yet implemented")
+        if (::translator.isInitialized)
+            translator.stop()
     }
 }
