e35ee26349686b0e76ab029c023175dcee8086f2
Emine INAN <emineinanx@gmail.com>
2024-03-22 15:43:13 +0300

    CMS-92 # Add javadoc to LocationSource.kt

--- mapkit/src/main/java/com/hms/lib/commonmobileservices/mapkit/LocationSource.kt
+++ mapkit/src/main/java/com/hms/lib/commonmobileservices/mapkit/LocationSource.kt
@@ -15,11 +15,31 @@ package com.hms.lib.commonmobileservices.mapkit
 
 import android.location.Location
 
+/**
+ * Interface for a location source that provides location updates.
+ */
 interface LocationSource {
+    /**
+     * Activates the location source and starts providing location updates.
+     *
+     * @param listener The listener to be notified when the location changes.
+     */
     fun activate(listener: OnLocationChangedListener)
+
+    /**
+     * Deactivates the location source and stops providing location updates.
+     */
     fun deactivate()
 
-    interface OnLocationChangedListener{
+    /**
+     * Interface definition for a callback to be invoked when the location changes.
+     */
+    interface OnLocationChangedListener {
+        /**
+         * Called when the location has changed.
+         *
+         * @param location The new location.
+         */
         fun onLocationChanged(location: Location)
     }
 }
