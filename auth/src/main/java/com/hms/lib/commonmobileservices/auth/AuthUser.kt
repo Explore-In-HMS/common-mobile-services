ff8f576770384917e1cd8e98795f180488aa51e7
Emine INAN <emineinanx@gmail.com>
2024-03-27 15:43:11 +0300

    CMS-103 # Add javadoc to AuthUser.kt

--- auth/src/main/java/com/hms/lib/commonmobileservices/auth/AuthUser.kt
+++ auth/src/main/java/com/hms/lib/commonmobileservices/auth/AuthUser.kt
@@ -17,6 +17,17 @@ package com.hms.lib.commonmobileservices.auth
 import com.hms.lib.commonmobileservices.auth.common.ProviderType
 import com.hms.lib.commonmobileservices.auth.common.ServiceType
 
+/**
+ * Data class representing an authenticated user.
+ *
+ * @property id The unique identifier of the user.
+ * @property displayName The display name of the user.
+ * @property email The email address of the user.
+ * @property phone The phone number of the user.
+ * @property photoUrl The URL of the user's profile photo.
+ * @property serviceType The type of mobile service used for authentication (GMS or HMS).
+ * @property providerType The type of authentication provider used (Google, Huawei, Facebook, Twitter, Email, Phone, etc.).
+ */
 data class AuthUser(
     val id: String,
     val displayName: String,
