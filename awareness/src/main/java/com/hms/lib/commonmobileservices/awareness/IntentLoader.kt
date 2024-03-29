c7bea0db4bdb79d86740ad88885a02c609f6119e
Emine INAN <emineinanx@gmail.com>
2024-03-29 15:57:28 +0300

    CMS-105 # Add javadoc to IntentLoader.kt

--- awareness/src/main/java/com/hms/lib/commonmobileservices/awareness/IntentLoader.kt
+++ awareness/src/main/java/com/hms/lib/commonmobileservices/awareness/IntentLoader.kt
@@ -18,11 +18,29 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 
-class IntentLoader(){
-    fun loadIntentWithExtrasOrReturnNull(context : Context,fullClassName: String,extras: Bundle): Intent? =
+/**
+ * Helper class to load an Intent with extras or return null if the class name is not found.
+ */
+class IntentLoader {
+    /**
+     * Loads an Intent with extras or returns null if the class name is not found.
+     *
+     * @param context The context to use for loading the Intent.
+     * @param fullClassName The fully qualified class name of the target component.
+     * @param extras The extras to be added to the Intent.
+     * @return An Intent with extras, or null if the class name is not found.
+     */
+    fun loadIntentWithExtrasOrReturnNull(
+        context: Context,
+        fullClassName: String,
+        extras: Bundle
+    ): Intent? =
         try {
-            Intent(Intent.ACTION_VIEW).apply { putExtras(extras) }.setClass(context, Class.forName(fullClassName))
+            // Constructing an Intent with the given extras and setting the class using the full class name
+            Intent(Intent.ACTION_VIEW).apply { putExtras(extras) }
+                .setClass(context, Class.forName(fullClassName))
         } catch (e: ClassNotFoundException) {
+            // If the class is not found, print the stack trace and return null
             e.printStackTrace()
             null
         }
