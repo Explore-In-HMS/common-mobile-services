4a0ad7b6a50621a03699b8d16f3c44710f3a25b0
Emine INAN <emineinanx@gmail.com>
2024-03-29 15:57:41 +0300

    CMS-105 # Add javadoc to TimeAwarenessData.kt

--- awareness/src/main/java/com/hms/lib/commonmobileservices/awareness/model/TimeAwarenessData.kt
+++ awareness/src/main/java/com/hms/lib/commonmobileservices/awareness/model/TimeAwarenessData.kt
@@ -16,19 +16,68 @@ package com.hms.lib.commonmobileservices.awareness.model
 
 import java.io.Serializable
 
+/**
+ * Represents awareness data related to time.
+ *
+ * @property timeDataArray A mutable list containing integer values representing time data.
+ * @constructor Creates an instance of [TimeAwarenessData] with the provided time data array.
+ */
 data class TimeAwarenessData(
     val timeDataArray: MutableList<Int>
-):Serializable
+) : Serializable
 
-enum class TimeDataValue(val value:Int) {
+/**
+ * Enumerates different values representing time categories.
+ *
+ * @property value The integer value associated with the time category.
+ */
+enum class TimeDataValue(val value: Int) {
+    /**
+     * Represents the morning time category.
+     */
     TIME_CATEGORY_MORNING(1),
+
+    /**
+     * Represents the afternoon time category.
+     */
     TIME_CATEGORY_AFTERNOON(2),
+
+    /**
+     * Represents the evening time category.
+     */
     TIME_CATEGORY_EVENING(3),
+
+    /**
+     * Represents the night time category.
+     */
     TIME_CATEGORY_NIGHT(4),
+
+    /**
+     * Represents the holiday time category.
+     */
     TIME_CATEGORY_HOLIDAY(5),
+
+    /**
+     * Represents the weekday time category.
+     */
     TIME_CATEGORY_WEEKDAY(6),
+
+    /**
+     * Represents the weekend time category.
+     */
     TIME_CATEGORY_WEEKEND(7),
+
+    /**
+     * Represents the time category that is not a holiday.
+     */
     TIME_CATEGORY_NOT_HOLIDAY(8);
+
+    /**
+     * Provides a way to retrieve a [TimeDataValue] enum instance based on its integer value.
+     *
+     * @param value The integer value associated with the time category.
+     * @return The [TimeDataValue] enum instance corresponding to the given value, or null if not found.
+     */
     companion object {
         fun valueOf(value: Int) = values().find { it.value == value }
     }
