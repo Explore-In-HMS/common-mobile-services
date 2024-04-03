893f6af08b0f782e8925f232f22dd6d626612f7d
f00834653 <feyza.urkut1@huawei.com>
2024-04-03 10:40:29 +0300

    CMS-116 # Add javadoc to GoogleImageLabeling.kt

--- imageclassification/src/main/java/com/hms/lib/commonmobileservices/imageclassification/implementation/GoogleImageLabeling.kt
+++ imageclassification/src/main/java/com/hms/lib/commonmobileservices/imageclassification/implementation/GoogleImageLabeling.kt
@@ -20,9 +20,24 @@ import com.hms.lib.commonmobileservices.core.ErrorModel
 import com.hms.lib.commonmobileservices.imageclassification.common.ClassificationResult
 import com.hms.lib.commonmobileservices.imageclassification.common.ImageLabel
 
+/**
+ * A class implementing image classification using Google's ML Kit Image Labeling API.
+ *
+ * This class implements the [IImageClassification] interface, providing methods to analyze images
+ * and obtain classification results using Google's ML Kit Image Labeling API.
+ *
+ * @property imageLabeler The ImageLabeler instance used for labeling images.
+ */
 class GoogleImageLabeling(
     private val imageLabeler: ImageLabeler
 ) : IImageClassification {
+    /**
+     * Analyzes the provided bitmap image and returns the classification results asynchronously.
+     *
+     * @param bitmap The bitmap image to be analyzed.
+     * @param callback A callback function to receive the classification result asynchronously.
+     * The result is encapsulated within a [ClassificationResult] object containing a list of [ImageLabel]s.
+     */
     override fun analyseImage(
         bitmap: Bitmap,
         callback: (classificationResult: ClassificationResult<List<ImageLabel>>) -> Unit
@@ -54,5 +69,9 @@ class GoogleImageLabeling(
             }
     }
 
+    /**
+     * Stops the image classification analyzer.
+     * This method is not implemented for Google's ML Kit Image Labeling API.
+     */
     override fun stopAnalyzer() {}
 }
