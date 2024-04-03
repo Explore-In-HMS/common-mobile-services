8083cb1a89aea3d0e7843bff74001fd583c0f372
Emine INAN <emineinanx@gmail.com>
2024-04-03 10:43:34 +0300

    CMS-118 # Add javadoc to DetectionResult.kt

--- languagedetection/src/main/java/com/hms/lib/commonmobileservices/languagedetection/common/DetectionResult.kt
+++ languagedetection/src/main/java/com/hms/lib/commonmobileservices/languagedetection/common/DetectionResult.kt
@@ -15,12 +15,27 @@ package com.hms.lib.commonmobileservices.languagedetection.common
 
 import com.hms.lib.commonmobileservices.core.ErrorModel
 
+/**
+ * Sealed class representing the result of a detection operation.
+ * @param T The type of data contained in the result.
+ */
 sealed class DetectionResult<out T> {
+
+    /**
+     * Represents a successful detection result.
+     * @property data The data resulting from the detection operation.
+     */
     data class Success<out T>(
         val data: T
-    ): DetectionResult<T>()
+    ) : DetectionResult<T>()
+
+    /**
+     * Represents an error that occurred during the detection operation.
+     * @property errorMessage The error message describing the failure.
+     * @property errorModel An optional error model providing additional details about the failure.
+     */
     data class Error(
         val errorMessage: String? = null,
         val errorModel: ErrorModel? = null
-    ): DetectionResult<Nothing>()
+    ) : DetectionResult<Nothing>()
 }
