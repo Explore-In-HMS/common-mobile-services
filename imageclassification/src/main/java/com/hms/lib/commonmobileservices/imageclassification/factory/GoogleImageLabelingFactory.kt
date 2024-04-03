8f93a37951012b709df5681fdd3a5dcdb8dabc9c
f00834653 <feyza.urkut1@huawei.com>
2024-04-03 10:40:42 +0300

    CMS-116 # Add javadoc to GoogleImageLabelingFactory.kt

--- imageclassification/src/main/java/com/hms/lib/commonmobileservices/imageclassification/factory/GoogleImageLabelingFactory.kt
+++ imageclassification/src/main/java/com/hms/lib/commonmobileservices/imageclassification/factory/GoogleImageLabelingFactory.kt
@@ -18,13 +18,31 @@ import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import com.hms.lib.commonmobileservices.imageclassification.implementation.GoogleImageLabeling
 import com.hms.lib.commonmobileservices.imageclassification.implementation.IImageClassification
 
+/**
+ * A factory class for creating instances of image classification using Google's Image Labeling API.
+ *
+ * This factory extends [ImageClassificationFactory] and provides methods to create instances of
+ * [IImageClassification] specifically for Google's Image Labeling API.
+ */
 class GoogleImageLabelingFactory : ImageClassificationFactory() {
+    /**
+     * Creates an instance of image classification using Google's Image Labeling API with default options.
+     *
+     * @return An instance of [IImageClassification] for performing image classification.
+     */
     override fun create(): IImageClassification {
         return GoogleImageLabeling(
             ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
         )
     }
 
+    /**
+     * Creates an instance of image classification using Google's Image Labeling API with custom confidence threshold.
+     *
+     * @param confidenceThreshold The confidence threshold to be applied for classification. It should be a value
+     * between 0.0 and 1.0.
+     * @return An instance of [IImageClassification] for performing image classification with the specified confidence threshold.
+     */
     override fun create(confidenceThreshold: Float): IImageClassification {
         val threshold = confidenceThreshold.coerceIn(0.0f, 1.0f)
         val options = ImageLabelerOptions.Builder()
