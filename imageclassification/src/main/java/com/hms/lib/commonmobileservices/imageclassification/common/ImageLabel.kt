2d73de6a62ff364ad70c20b993c701af1a624258
f00834653 <feyza.urkut1@huawei.com>
2024-04-03 10:41:56 +0300

    CMS-116 # Add javadoc to ImageLabel.kt

--- imageclassification/src/main/java/com/hms/lib/commonmobileservices/imageclassification/common/ImageLabel.kt
+++ imageclassification/src/main/java/com/hms/lib/commonmobileservices/imageclassification/common/ImageLabel.kt
@@ -1,5 +1,12 @@
 package com.hms.lib.commonmobileservices.imageclassification.common
 
+/**
+ * A data class representing a labeled image along with the possibility score of the label.
+ *
+ * @property name The name or label of the image.
+ * @property possibility The possibility score associated with the image label, indicating the likelihood
+ * of the label being accurate or present in the image.
+ */
 data class ImageLabel(
     val name: String,
     val possibility: Float
