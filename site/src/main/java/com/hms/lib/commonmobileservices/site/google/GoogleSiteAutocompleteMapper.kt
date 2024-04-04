acbc0e7a2ca35dec06c9d906aad9229c1a3b1fad
f00834653 <feyza.urkut1@huawei.com>
2024-04-04 08:48:20 +0300

    CMS-130 # Add javadoc to GoogleSiteAutocompleteMapper.kt

--- site/src/main/java/com/hms/lib/commonmobileservices/site/google/GoogleSiteAutocompleteMapper.kt
+++ site/src/main/java/com/hms/lib/commonmobileservices/site/google/GoogleSiteAutocompleteMapper.kt
@@ -18,45 +18,53 @@ import com.hms.lib.commonmobileservices.site.SiteServiceReturn
 import com.hms.lib.commonmobileservices.site.common.Mapper
 import org.json.JSONObject
 
+/**
+ * Mapper class for mapping JSONObjects to SiteServiceReturn objects for Google site autocomplete.
+ */
 class GoogleSiteAutocompleteMapper: Mapper<SiteServiceReturn, JSONObject>() {
+
+    /**
+     * Maps a JSONObject to a SiteServiceReturn object.
+     *
+     * @param from The JSONObject to be mapped.
+     * @return The mapped SiteServiceReturn object.
+     */
     override fun mapToEntity(from: JSONObject): SiteServiceReturn = SiteServiceReturn(
-        id = if (from.has("place_id")){from.getString("place_id")} else {"No Place ID Info"},
+        id = if (from.has("place_id")) { from.getString("place_id") } else { "No Place ID Info" },
         name = from.getJSONObject("structured_formatting").getString("main_text"),
         locationLat = null,
         locationLong = null,
-        phoneNumber = if(from.has("international_phone_number")){
+        phoneNumber = if (from.has("international_phone_number")) {
             from.getString("international_phone_number")
-        }
-        else{
+        } else {
             "No phone Number"
         },
-        formatAddress = if(from.has("description")){
+        formatAddress = if (from.has("description")) {
             from.getString("description")
-        }else{
+        } else {
             "No Formatted Address"
         },
         distance = null,
-        image = if (from.has("photos")){
+        image = if (from.has("photos")) {
             val array = from.getJSONArray("photos")
             val arrayList : ArrayList<String> = ArrayList()
-            for (i in 0 until array.length()){
+            for (i in 0 until array.length()) {
                 val currentObject = array.getJSONObject(i)
                 arrayList.add(currentObject.getString("photo_reference"))
             }
             arrayList
-        }
-        else{
+        } else {
             ArrayList()
         },
-        averagePrice = if (from.has("price_level")){
+        averagePrice = if (from.has("price_level")) {
             from.getString("price_level").toDouble()
-        }else{
+        } else {
             0.00
         },
         point = if (from.has("rating")) {
             from.getString("rating").toDouble()
-        }else{
+        } else {
             0.00
-        },
+        }
     )
 }
