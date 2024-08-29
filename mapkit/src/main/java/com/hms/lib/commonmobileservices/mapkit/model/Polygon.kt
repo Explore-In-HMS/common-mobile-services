674c69a790aa84f689bd14f82a2c23fd69f59bd1
Emine INAN <emineinanx@gmail.com>
2024-08-29 10:21:45 +0300

    CMS-196 # Remove unreachable type casts

--- mapkit/src/main/java/com/hms/lib/commonmobileservices/mapkit/model/Polygon.kt
+++ mapkit/src/main/java/com/hms/lib/commonmobileservices/mapkit/model/Polygon.kt
@@ -80,8 +80,10 @@ class Polygon(val polygonImpl: Any) {
     fun setPoints(points: List<LatLng?>?) {
         when (polygonImpl) {
             is HmsPolygon -> polygonImpl.points = points?.map { it?.toHMSLatLng() }
-            is GmsPolygon -> polygonImpl.points =
-                points?.map { it?.toGMSLatLng() } as MutableList<com.google.android.gms.maps.model.LatLng>
+            is GmsPolygon -> {
+                polygonImpl.points =
+                    points?.map { it?.toGMSLatLng() }?.toMutableList() ?: mutableListOf()
+            }
         }
     }
 
@@ -107,8 +109,10 @@ class Polygon(val polygonImpl: Any) {
         when (polygonImpl) {
             is HmsPolygon -> polygonImpl.holes =
                 holes?.map { list -> list?.map { item -> item?.toHMSLatLng() } }
+
             is GmsPolygon -> polygonImpl.holes =
-                holes?.map { list -> list?.map { item -> item?.toGMSLatLng() } } as MutableList<MutableList<com.google.android.gms.maps.model.LatLng>>
+                holes?.map { list -> list?.map { item -> item?.toGMSLatLng() } }?.toMutableList()
+                    ?: mutableListOf()
         }
     }
 
